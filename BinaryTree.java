264e99def6f137af9058f26c3752b50a089bc7c8
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-07-25 19:17:57 +0900

    add と showのを押したときのオプションを追加

--- BinaryTree.java
+++ BinaryTree.java
@@ -5,11 +5,40 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.lang.*;
 class BinaryTree {
-  BinaryTreeNode root;
+  BinaryTreeNode root, newTree;
 
   BinaryTree(Object data) {
     root = new BinaryTreeNode(data, null, null);
   }
+//----------------------------------------------------------------------
+  // void insert(Object data) {
+  //   newTree = new Cell(data);
+  //   Cell pastCell = header;
+  //   while(isNextCellExist(pastCell)){
+  //     if(isNextCellDataInsertData(pastCell, data)){
+  //       if(isNextCellLast(pastCell)){
+  //         break;
+  //       }else{
+  //         pastCell.next = pastCell.next.next;
+  //       }
+  //     }
+  //     pastCell = pastCell.next;
+  //   }
+  //   pastCell.next = newCell;
+  // }
+
+  // private boolean isNextCellExist(Cell cell){
+  //   return cell.next != null;
+  // }
+
+  // private boolean isNextCellDataInsertData(Cell cell, Object data){
+  //   return cell.next.data.equals(data);
+  // }
+
+  // private boolean isNextCellLast(Cell cell){
+  //   return cell.next.next == null;
+  // }
+//----------------------------------------------------------------------
 
   String printTree(){
     String s = root.data + "";
@@ -26,16 +55,17 @@ class BinaryTree {
     while(isContinue){
       try{
         BufferedReader input;
-        System.out.println("input:");
+        System.out.println("option(add/show)");
         input = new BufferedReader (new InputStreamReader (System.in));
         value = input.readLine( );
         if(value.equals("end")){
           isContinue = false;
         }else if(value.equals("") || value.indexOf(" ") >= 0 || value.indexOf("　") >= 0){
           System.out.println("空白文字は含まないで下さい");
-        }else{
-          // s.insert(value);
-          System.out.println(tree.printTree());
+        }else if(value.equals("show")){
+          System.out.println("treeを表示します");
+        }else if(value.equals("add")){
+          System.out.println("treeを追加します");
         }
       }catch(IOException e){
         e.printStackTrace();
